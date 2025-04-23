c3ceb4e9efae664fdff4f673a5bd73db83784c6d
Jakub Havlas <jakub.havlas.022@pslib.cz>
2025-04-23 10:59:44 +0200

    ads

--- src/components/types.tsx
+++ src/components/types.tsx
@@ -1,17 +1,22 @@
 export type FunctionData = {
-    id : number;
-    color: string;
-    expression: string[];
-    pathArray: coords[][];
-  };
+  id: number;
+  color: string;
+  expression: string[];
+  pathArray: coords[][];
+};
 
- export type ViewBox = {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-  };
-  export type coords = {
-    x: number;
-    y: number;
-  };
+export type ViewBox = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+export type coords = {
+  x: number;
+  y: number;
+};
+
+export type reqs = {
+  expression: string;
+  color: string;
+};
